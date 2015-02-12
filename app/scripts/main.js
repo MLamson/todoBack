62bb0b25b8dc37d3453e598126ace7d0531f110a
Mark Lamson <lamson_mark@yahoo.com>
2015-02-12 14:48:02 -0500

    get input into array with backbone

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -34,8 +34,69 @@ var ItemCol = Backbone.Collection.extend({
 
 });
 
-var allItems = new ItemCol();
 
-var a = new Item ({name: 'Milk'});
-var b = new Item ({name: 'Eggs'});
-var c = new Item ({name: 'Bread'});
+var itemCol = new ItemCol();
+////////////////////////////////////
+//////////////////////////////////////
+//get text from field when button in form clicked.
+$('#itemForm').on('submit', function(e){
+	e.preventDefault();
+	console.log('click');
+
+	var itemName = document.getElementById('text').value;
+    console.log('gettext');
+    console.log(itemName);
+    // Create a new Todo
+    var itemInstance = new Item({name: itemName});
+
+    itemCol.add(itemInstance);
+
+    // Clear the form
+    //taskname.reset();
+    $("#itemForm")[0].reset();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////
+///////////////////////////////////
+//_.template code
+var todoTemplate = $('#todoTemp').html();
+var todoTemplateFunc = _.template(todoTemplate);
+
+ addItem = function (item) {
+      	console.log(item);
+   // todoArray.push(item);
+    $('#addItemHere').prepend(todoTemplateFunc(item));
+  };
+//////////////////////////////////////
+////////////////////////////////////////////
+// var allItems = new ItemCol();
+
+// var a = new Item ({name: 'Milk'});
+// var b = new Item ({name: 'Eggs'});
+// var c = new Item ({name: 'Bread'});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
