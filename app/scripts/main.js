b089d6d458b6d18a86fb3ce97806186466b9489f
Mark Lamson <lamson_mark@yahoo.com>
2015-02-12 13:48:09 -0500

    rough out Backbone and HTML form

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,41 @@
 console.log('The Iron Yard Rocks');
+
+var Item = Backbone.Model.extend({
+
+		initialize: function(){
+
+			console.log('New Item created');
+			console.log(this);
+		},
+
+		idAttribute: '_id',
+
+		defaults: {
+			name: '',
+			status: 'incomplete'
+		
+		}
+
+});
+
+var ItemCol = Backbone.Collection.extend({
+
+	initialize: function () {
+		console.log('Item collection Created');
+		console.log(this);
+	},
+
+	model: Item,
+
+
+
+	url: 'http://tiy-atl-fe-server.herokuapp.com/collections/backboneMark',
+
+
+});
+
+var allItems = new ItemCol();
+
+var a = new Item ({name: 'Milk'});
+var b = new Item ({name: 'Eggs'});
+var c = new Item ({name: 'Bread'});
